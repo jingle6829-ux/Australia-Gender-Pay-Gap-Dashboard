9a356e75031d93e121968dd641ea85f3f47e2017
jingle6829-ux <jingle6829@gmail.com>
2026-09-24 00:45:41 +1000

    Update file path for raw data in dashboard script

--- Australia-Gender-Pay-Gap-Dashboard.py
+++ Australia-Gender-Pay-Gap-Dashboard.py
@@ -11,7 +11,7 @@ import plotly.express as px
 # ===========================
 # Loading Data
 # ============================
-file_path = '/Users/jingle/Downloads/QBUS5010_PROJECT_GROUP_011/raw data.xlsx'
+file_path = '/Users/jingle/Downloads/Australia-Gender-Pay-Gap-Dashboard/raw data.xlsx'
 
 sheet_names = ['state(Weekly Cash earning)', 'population&working hours']
 
